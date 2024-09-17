30bb359cafabcc5535fd2afcfacb41fe347c39f0
dzmat <dzmat@yandex.ru>
2024-09-17 20:55:43 +0700

    reorder TWay declaration lines

--- tway.h
+++ tway.h
@@ -10,6 +10,13 @@
 class TWay
 {
 public:
+    typedef struct TWayPoint
+    {
+        int waypoint_index;
+        TGameCoord coord;
+        double time_of_arrival = -100;
+    } TWayPoint;
+    std::vector<TWayPoint> data;
     TWay()
     {
     }
@@ -18,17 +25,8 @@ public:
     {
     }
 
-    typedef struct TWayPoint
-    {
-        int waypoint_index;
-        TGameCoord coord;
-        double time_of_arrival = -100;
-    } TWayPoint;
     int next_way_point = 0;
     void load();
-    //    size_t size() const {return data.size();}
-    size_t size() const {return data.size();}
-    std::vector<TWayPoint> data;
     double min_distance_to(const TGameCoord &dest) const;
 };
 
